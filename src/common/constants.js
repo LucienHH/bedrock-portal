7a5a3c8db5485a778ed23019c589a74d197df89a
unknown <lucienharryholloway@hotmail.co.uk>
2023-02-04 19:22:10 +0000

    Update constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,29 +1,35 @@
+const Endpoints = {
+  Handle: 'https://sessiondirectory.xboxlive.com/handles',
+  ServiceConfigs: 'https://sessiondirectory.xboxlive.com/serviceconfigs',
+};
+
+const SessionConfig = {
+  MinecraftTitleID: '896928775',
+  MinecraftSCID: '4fc10100-5f7a-4470-899b-280835760c07',
+  MinecraftTemplateName: 'MinecraftLobby',
+  MiencraftProtocolVersion: 560,
+};
+
+const Joinability = {
+  invite_only: {
+    joinability: 'invite_only',
+    joinRestriction: 'local',
+    broadcastSetting: 1,
+  },
+  friends_only: {
+    joinability: 'joinable_by_friends',
+    joinRestriction: 'followed',
+    broadcastSetting: 2,
+  },
+  friends_of_friends: {
+    joinability: 'joinable_by_friends',
+    joinRestriction: 'followed',
+    broadcastSetting: 3,
+  },
+};
+
 module.exports = {
-  Endpoints: {
-    Handle: 'https://sessiondirectory.xboxlive.com/handles',
-    ServiceConfigs: 'https://sessiondirectory.xboxlive.com/serviceconfigs',
-  },
-  SessionConfig: {
-    MinecraftTitleID: '896928775',
-    MinecraftSCID: '4fc10100-5f7a-4470-899b-280835760c07',
-    MinecraftTemplateName: 'MinecraftLobby',
-    MiencraftProtocolVersion: 560,
-  },
-  Joinability: {
-    invite_only: {
-      joinability: 'invite_only',
-      joinRestriction: 'local',
-      broadcastSetting: 1,
-    },
-    friends_only: {
-      joinability: 'joinable_by_friends',
-      joinRestriction: 'followed',
-      broadcastSetting: 2,
-    },
-    friends_of_friends: {
-      joinability: 'joinable_by_friends',
-      joinRestriction: 'followed',
-      broadcastSetting: 3,
-    },
-  },
+  Endpoints,
+  SessionConfig,
+  Joinability,
 };
