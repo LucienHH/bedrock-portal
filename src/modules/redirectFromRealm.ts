ae2560f71e10ad1242581174668b8c191039197d
unknown <lucienharryholloway@hotmail.co.uk>
2024-10-09 17:46:30 +0100

    Add `overideDeviceOS` option to `RedirectFromRealm` module

--- src/modules/redirectFromRealm.ts
+++ src/modules/redirectFromRealm.ts
@@ -20,6 +20,11 @@ export default class RedirectFromRealm extends Module {
      */
     clientOptions: any,
     /**
+     * The device OS to use when connecting to the Realm. This is passed directly to a [bedrock-protocol createClient](https://github.com/PrismarineJS/bedrock-protocol/blob/master/docs/API.md#becreateclientoptions--client)
+     * @default 7 // = Windows
+    */
+    overideDeviceOS: number,
+    /**
      * Options for the chat command
      * @type {object}
      */
@@ -46,6 +51,7 @@ export default class RedirectFromRealm extends Module {
     super('redirectFromRealm', 'Automatically invite players to the server when they join a Realm')
     this.options = {
       clientOptions: { }, // Options for the bedrock-protocol client
+      overideDeviceOS: 7,
       chatCommand: {
         enabled: true,
         cooldown: 60000,
@@ -76,6 +82,8 @@ export default class RedirectFromRealm extends Module {
     return new Promise<any>((resolve, reject) => {
 
       this.client = this.bedrock.createClient(options)
+
+      if (this.options.overideDeviceOS) this.client.session.deviceOS = this.options.overideDeviceOS
 
       this.client.once('error', (e: any) => (this.client = null, reject(e)))
 
