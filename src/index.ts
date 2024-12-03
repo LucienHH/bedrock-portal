dc6394b63e05bcd6d5ca06ca3231d27d1b348f9d
unknown <lucienharryholloway@hotmail.co.uk>
2024-12-03 21:22:44 +0000

    Release v0.9.2

--- src/index.ts
+++ src/index.ts
@@ -149,9 +149,9 @@ export class BedrockPortal extends TypedEmitter<PortalEvents> {
       webRTCNetworkId: getRandomUint64(),
       ...options,
       world: {
-        hostName: 'Bedrock Portal v0.6.0',
+        hostName: 'Bedrock Portal v0.9.2',
         name: 'By LucienHH',
-        version: '0.7.1',
+        version: '0.9.2',
         memberCount: 0,
         maxMemberCount: 10,
         ...options.world,
