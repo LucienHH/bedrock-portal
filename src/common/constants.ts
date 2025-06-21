9ef91aa5a3d88401152418ba2d4e3adcd1c751d5
LucienHH <lucienharryholloway@hotmail.co.uk>
2025-06-21 11:32:48 +0100

    Add support for v1.21.90

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,7 +2,7 @@ export const SessionConfig = {
   MinecraftTitleID: '896928775',
   MinecraftSCID: '4fc10100-5f7a-4470-899b-280835760c07',
   MinecraftTemplateName: 'MinecraftLobby',
-  MiencraftProtocolVersion: 800,
+  MiencraftProtocolVersion: 818,
 }
 
 export enum Joinability {
