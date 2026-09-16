94213f7454cb6402950bc6b34cc1d41c137d9f27
Zephyr-OP <57580080+Zephyr-OP@users.noreply.github.com>
2026-09-16 09:48:07 -0600

    Support v1.26.50

--- src/common/start_game.ts
+++ src/common/start_game.ts
@@ -82,7 +82,7 @@ export const start_game = {
   block_properties: [],
   multiplayer_correlation_id: '<raknet>b31f-884c-6f27-c0fb',
   server_authoritative_inventory: true,
-  engine: '1.26.40',
+  engine: '1.26.50',
   property_data: {
     type: 'compound',
     name: '',
